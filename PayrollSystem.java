be1592c221de142629cdc0c4b639d8d242748efe
TusharJain86 <tusharjain0806@gmail.com>
2025-10-22 13:10:52 +0530

    update emppayroll

--- PayrollSystem.java
+++ PayrollSystem.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class PayrollSystem {
-    
+    private ArrayList<Employee> employeeList;
+
+    public PayrollSystem() {
+        employeeList = new ArrayList<>();
+    }
+
+    public void addEmployee(Employee employee) {
+        employeeList.add(employee);
+    }
 }
